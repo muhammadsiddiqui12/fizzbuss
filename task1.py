d151007bb16bcbd17311d28d7368840ade6bec7a
muhammadsiddiqui <mh769842@dal.ca>
2019-05-23 06:11:01 -0700

    initialising functions and requiremtns to program

--- task1.py
+++ task1.py
@@ -2,3 +2,11 @@ filename = input("Enter filename")
 pages = input("Enter pages you want to copy(seperated by commas)")
 pageslist = pages.split(",")
 lineNumber = 0
+
+
+def isLineInPage():
+    for x in pageslist:
+        lowerLimit = 1 + ((int(x) - 1) * 25)
+        upperLimit = 25 + ((int(x) - 1) * 25)
+        if lineNumber >= lowerLimit and lineNumber <= upperLimit:
+            return True
